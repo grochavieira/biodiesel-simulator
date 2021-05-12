0d667d6f028b0d996c4a7e7413cb1334240c83d2
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-05-11 22:27:02 -0300

    almost finished

--- main.cpp
+++ main.cpp
@@ -17,7 +17,19 @@ float oleo;
 float reator[3] = {0, 0, 0};
 float decantador = 0;
 float glicerina = 0;
+float biodiesel = 0;
+float secadorSolucao = 0;
 float solucaoLavagem = 0;
+bool lavagemAtiva[3] = {false, false, false};
+float lavagem1 = 0;
+float lavagem2 = 0;
+float lavagem3 = 0;
+bool decantadorAtivo = true;
+float secadorEtOH = 0;
+bool secadorEtohAtivo = true;
+bool secadorBiodieselAtivo = false;
+bool tanqueReatorEncheu = false;
+int ciclos = 0;
 
 pthread_cond_t condicao[Componentes];
 
@@ -30,9 +42,9 @@ void tanqueGlicerina();
 void secadorDecantador();
 void tanqueEtOH();
 void tanqueNaOH_EtOH();
-void lavagem1();
-void lavagem2();
-void lavagem3();
+void tanqueLavagem1();
+void tanqueLavagem2();
+void tanqueLavagem3();
 void secadorBiodiesel();
 void tanqueBiodiesel();
 
@@ -72,6 +84,11 @@ void *threadComponente(void *arg)
         if (valorThread == 0)
         {
             // Orquestrador
+            ciclos++;
+            usleep(1000 * 2000);
+            cout << "Ciclo " << ciclos << endl;
+            cout << "Biodiesel => " << biodiesel << " | Glicerina => " << glicerina << " | Óleo => " << oleo << " | NaOH => " << naoh << " | EtOh => " << etoh << endl;
+            cout << "***********************************************" << endl;
         }
         if (valorThread == 1)
         {
@@ -95,40 +112,32 @@ void *threadComponente(void *arg)
         }
         if (valorThread == 5)
         {
-            // Tanque de Glicerina
-        }
-        if (valorThread == 6)
-        {
             // Secador
         }
-        if (valorThread == 7)
-        {
-            // Tanque de EtOH
-        }
-        if (valorThread == 8)
+        if (valorThread == 6)
         {
             // Tanque de NaOH e EtOH
             tanqueNaOH_EtOH();
         }
-        if (valorThread == 9)
+        if (valorThread == 7)
         {
             // Lavagem 1 (Emulsão)
+            tanqueLavagem1();
+        }
+        if (valorThread == 8)
+        {
+            // Lavagem 2
+            tanqueLavagem2();
+        }
+        if (valorThread == 9)
+        {
+            // Lavagem 3 (Água)
+            tanqueLavagem3();
         }
         if (valorThread == 10)
         {
-            // Lavagem 2
-        }
-        if (valorThread == 11)
-        {
-            // Lavagem 3 (Água)
-        }
-        if (valorThread == 12)
-        {
-            // Secador Final
-        }
-        if (valorThread == 13)
-        {
-            // Tanque de Biodiesel
+            // Secador Biodiesel
+            secadorBiodiesel();
         }
     }
 
@@ -142,8 +151,8 @@ void oleoResidualFritura()
     float aux = rand() % 1000;
     float cargaOleo = 1.0 + (aux / 1000);
     oleo += cargaOleo;
-    cout << "Enviando carga de óleo: " << cargaOleo << endl;
-    cout << "Óleo: " << oleo << endl;
+    //cout << "Enviando carga de óleo: " << cargaOleo << endl;
+    //cout << "Óleo: " << oleo << endl;
     usleep((1000 * 10000) - tempo);
 }
 
@@ -160,71 +169,64 @@ void tanqueOleo()
 
 void tanqueReator()
 {
-    bool transferiuConteudo = false;
+
+    //cout << "Óleo do Reator: " << reator[0] << endl;
+    //cout << "NaOH do Reator: " << reator[1] << endl;
+    //cout << "EtOH do Reator: " << reator[2] << endl;
+
     if (reator[0] >= 1.25 && reator[1] >= 1.25 && reator[2] >= 2.50)
     {
-        cout << "ENCHEU" << endl;
-
-        if (decantador < 10.0)
-        {
-            float decantadorCopia = decantador;
-            float restanteDecantador = 10.0 - decantador;
-            if (restanteDecantador >= 3.0)
-            {
-                decantador += 3.0;
-                reator[0] = 1.25 / (3 / 5);
-                reator[1] = 1.25 / (3 / 5);
-                reator[2] = 2.50 / (3 / 5);
-            }
-            else
-            {
-                decantador += restanteDecantador;
-                reator[0] = 1.25 / (restanteDecantador / 5);
-                reator[1] = 1.25 / (restanteDecantador / 5);
-                reator[2] = 2.50 / (restanteDecantador / 5);
-            }
-
-            cout << "Enviou para o decantador" << endl;
-            transferiuConteudo = true;
-        }
+        //cout << "Tanque do Reator ENCHEU" << endl;
+        tanqueReatorEncheu = true;
     }
-    cout << "Óleo do Reator: " << reator[0] << endl;
-    cout << "NaOH do Reator: " << reator[1] << endl;
-    cout << "EtOH do Reator: " << reator[2] << endl;
 
-    if (!transferiuConteudo)
-    {
-        usleep(1000 * 1000);
-    }
-    else
-    {
-        usleep(1000 * 5000);
-        transferiuConteudo = false;
-    }
+    usleep(1000 * 1000);
 }
 
 void tanqueDecantador()
 {
-    if (decantador == 10.0)
+    if (decantador < 10.0 && tanqueReatorEncheu)
     {
+        float decantadorCopia = decantador;
+        float restanteDecantador = 10.0 - decantador;
+
+        decantador += 5.0;
+        reator[0] = 0;
+        reator[1] = 0;
+        reator[2] = 0;
+
+        //cout << "PAUSA DO DECANTADOR" << endl;
+
+        usleep(1000 * 5000 * 1.66);
+
+        //cout << "Enviou para o decantador" << endl;
+        //cout << "VALOR DECANTADOR => " << decantador << endl;
+        tanqueReatorEncheu = false;
+    }
+
+    if (decantador > 0 && decantadorAtivo == true)
+    {
+        //cout << endl << "DECANTADOR" << endl;
         glicerina += decantador * 0.02;
-        etoh += decantador * 0.09;
+        secadorEtOH += decantador * 0.09;
         solucaoLavagem += decantador * 0.89;
 
         decantador = 0;
+        lavagemAtiva[0] = true;
+        secadorEtohAtivo = true;
+        decantadorAtivo = false;
     }
-}
-
-void tanqueGlicerina()
-{
 }
 
 void secadorDecantador()
 {
-}
-
-void tanqueEtOH()
-{
+    if (secadorEtohAtivo == true)
+    {
+        etoh += secadorEtOH - (secadorEtOH * 0.03);
+        secadorEtOH = 0;
+        secadorEtohAtivo = false;
+        usleep(1000 * 5000);
+    }
 }
 
 void tanqueNaOH_EtOH()
@@ -233,8 +235,8 @@ void tanqueNaOH_EtOH()
     naoh += 0.25;
     etoh += 0.125;
 
-    cout << "NaOH: " << naoh << endl;
-    cout << "EtOH: " << etoh << endl;
+    //cout << "NaOH: " << naoh << endl;
+    //cout << "EtOH: " << etoh << endl;
 
     if (reator[1] < 1.25 && naoh >= 1.25)
     {
@@ -252,22 +254,64 @@ void tanqueNaOH_EtOH()
     }
 }
 
-void lavagem1()
+void tanqueLavagem1()
 {
+    if (lavagemAtiva[0] && solucaoLavagem > 0)
+    {
+        decantadorAtivo = false;
+        lavagem1 += solucaoLavagem - (solucaoLavagem * 0.075);
+        solucaoLavagem -= solucaoLavagem * 0.075;
+        lavagemAtiva[0] = false;
+        usleep(1000 * 1000);
+        lavagemAtiva[1] = true;
+        //cout << "LAVAGEM 1: " << lavagem1 << endl;
+    }
 }
 
-void lavagem2()
+void tanqueLavagem2()
 {
+    if (lavagemAtiva[1])
+    {
+        lavagem1 = 0;
+        lavagem2 += solucaoLavagem - (solucaoLavagem * 0.075);
+        solucaoLavagem -= solucaoLavagem * 0.075;
+        lavagemAtiva[1] = false;
+        usleep(1000 * 1000);
+        lavagemAtiva[2] = true;
+        //cout << "LAVAGEM 2: " << lavagem2 << endl;
+    }
 }
 
-void lavagem3()
+void tanqueLavagem3()
 {
+    if (lavagemAtiva[2])
+    {
+        lavagem2 = 0;
+        lavagem3 += solucaoLavagem - (solucaoLavagem * 0.075);
+        solucaoLavagem -= solucaoLavagem * 0.075;
+        lavagemAtiva[2] = false;
+        //cout << "LAVAGEM 3: " << lavagem3 << endl;
+        usleep(1000 * 1000);
+        secadorBiodieselAtivo = true;
+        secadorSolucao = lavagem3;
+        lavagem3 = 0;
+    }
 }
 
 void secadorBiodiesel()
 {
+    if (secadorBiodieselAtivo)
+    {
+        biodiesel += secadorSolucao - (secadorSolucao * 0.03);
+        //cout << "ENVIANDO SOLUÇÃO DO SECADOR..." << endl;
+        usleep(1000 * secadorSolucao * 5000);
+        //cout << endl << "************************" << endl;
+        //cout << "ENVIOU PARA O BIODIESEL" << endl;
+        //cout << "************************" << endl;
+        secadorBiodieselAtivo = false;
+        decantadorAtivo = true;
+    }
 }
-
-void tanqueBiodiesel()
-{
-}
+// TO DO
+// Apresentar uma mensagem quando os componentes mudarem do valor 0 para
+// alguma coisa
